f73f8281630775c14340f2ef7d33bd2e8ffb619e
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-03-05 19:43:46 +0530

    Learn Appply CSS in angular

--- DAY-4/counterapp/src/app/components/user-profile/user-profile.component.ts
+++ DAY-4/counterapp/src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+//decorater
 @Component({
   selector: 'app-user-profile',
   standalone: true,
@@ -7,6 +9,10 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
+//compomnet whole ko kahte hai
 export class UserProfileComponent {
-
+name="Rakesh"
+age=25
+salary=120000
+status="single"
 }

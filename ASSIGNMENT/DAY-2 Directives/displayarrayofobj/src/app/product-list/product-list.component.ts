e7514a4772fadb407cb79389954b7add79b269a5
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-03-12 11:47:38 +0530

    use ngFor,ngIf directive

---
+++ ASSIGNMENT/DAY-2 Directives/displayarrayofobj/src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+})
+export class ProductListComponent {
+  products = [
+    { name: 'Laptop', price: 1200, available: true },
+    { name: 'Smartphone', price: 800, available: true },
+    { name: 'Headphones', price: 100, available: false },
+    { name: 'Camera', price: 600, available: true },
+    { name: 'Tablet', price: 300, available: false },
+    { name: 'Fitness Tracker', price: 50, available: true },
+    { name: 'Bluetooth Speaker', price: 80, available: true },
+    { name: 'Gaming Console', price: 400, available: false },
+    { name: 'Smartwatch', price: 150, available: true },
+    { name: 'External Hard Drive', price: 120, available: true },
+  ];
+}

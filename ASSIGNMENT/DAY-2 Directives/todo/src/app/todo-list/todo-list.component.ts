f6ff21a6782810a6bf7545d38d39b7bcfc8e9357
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-03-12 16:46:28 +0530

    Comleted Todo App-use ngfor,ngif,ng-template,ng-container

---
+++ ASSIGNMENT/DAY-2 Directives/todo/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css',
+})
+export class TodoListComponent {
+  todos = [
+    {
+      title: 'Task 1',
+      description: 'Description for Task 1',
+      completed: false,
+    },
+    { title: 'Task 2', description: 'Description for Task 2', completed: true },
+    {
+      title: 'Task 3',
+      description: 'Description for Task 3',
+      completed: false,
+    },
+    { title: 'Task 4', description: 'Description for Task 4', completed: true },
+    {
+      title: 'Task 5',
+      description: 'Description for Task 5',
+      completed: false,
+    },
+    { title: 'Task 6', description: 'Description for Task 6', completed: true },
+    {
+      title: 'Task 7',
+      description: 'Description for Task 7',
+      completed: false,
+    },
+    { title: 'Task 8', description: 'Description for Task 8', completed: true },
+    {
+      title: 'Task 9',
+      description: 'Description for Task 9',
+      completed: false,
+    },
+    {
+      title: 'Task 10',
+      description: 'Description for Task 10',
+      completed: true,
+    },
+  ];
+}

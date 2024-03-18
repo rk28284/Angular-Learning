1f7928861ef4497d11ab236c6e3e2afbad42a924
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-03-18 19:44:51 +0530

    Learn routing

---
+++ DAY-7/app-raouting/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UsersComponent } from './users/users.component';
+
+export const routes: Routes = [
+    {path:"home",component:HomeComponent},
+    {path:"about",component:AboutComponent}
+,
+{path:"user",component:UsersComponent}
+];

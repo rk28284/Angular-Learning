c71a75950c1030eae5f33e85d720f7f685c87863
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-03-20 12:14:02 +0530

    Authetication using ls completed

---
+++ ASSIGNMENT/DAY-5/autheticationls/src/app/component/feedbackform/feedbackform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedbackform',
+  standalone: true,
+  imports: [],
+  templateUrl: './feedbackform.component.html',
+  styleUrl: './feedbackform.component.css'
+})
+export class FeedbackformComponent {
+
+}
